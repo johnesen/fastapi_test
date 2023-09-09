90501c7171ea01e8edc9b47776e2942515cd0931
nurj0hn <nurjantunteev@gmail.com>
2023-09-09 22:09:09 +0600

    change files

---
+++ src/app.py
@@ -0,0 +1,30 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.wsgi import WSGIMiddleware
+from flask import Flask
+from flask_admin import Admin
+from sqlalchemy.orm import Session
+
+from admin.user_views import UserView
+from config.db_config import sync_engine
+from model.user_model import User
+from routers import router
+
+db = Session(sync_engine)
+
+flask_app = Flask(__name__)
+flask_app.config["SECRET_KEY"] = "secret"
+
+app = FastAPI(title="some peace of sh...  test", redoc_url="/redoc")
+
+admin = Admin(flask_app, template_mode="bootstrap4")
+
+# register user view
+admin.add_view(UserView(User, db))
+
+# mount flast/flask-admin
+app.mount("", WSGIMiddleware(flask_app))
+app.mount("/admin", WSGIMiddleware(admin.app))
+
+# register all routes
+app.include_router(router)
--- src/server.py
+++ src/server.py
@@ -1,31 +1,4 @@
 import uvicorn
-from fastapi import FastAPI
-from fastapi.middleware.wsgi import WSGIMiddleware
-from flask import Flask
-from flask_admin import Admin
-from sqlalchemy.orm import Session
-
-from admin.user_views import UserView
-from api import router
-from config.db_config import sync_engine
-from model.user_model import User
-
-db = Session(sync_engine)
-
-flask_app = Flask(__name__)
-flask_app.config["SECRET_KEY"] = "secret"
-
-app = FastAPI(title="some peace of sh...  test", redoc_url="/redoc")
-
-admin = Admin(flask_app, template_mode="bootstrap4")
-
-
-admin.add_view(UserView(User, db))
-app.mount("", WSGIMiddleware(flask_app))
-app.mount("/admin", WSGIMiddleware(admin.app))
-
-app.include_router(router)
-
 
 if __name__ == "__main__":
-    uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
