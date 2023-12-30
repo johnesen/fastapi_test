b48848f4de05d8378b41f8b32529d18ed90e9939
nurj0hn <nurjantunteev@gmail.com>
2023-12-30 21:51:23 +0600

    pylint

--- src/router/user_router.py
+++ src/router/user_router.py
@@ -2,6 +2,7 @@ from typing import List
 
 from config import get_db
 from fastapi import BackgroundTasks, Depends
+from typing import Annotated
 from fastapi.routing import APIRouter
 from model import User
 from schema import ShowUser, UpdateUser, UserCreate
@@ -9,6 +10,8 @@ from service import AuthService, SendEmailService, UserService
 from sqlalchemy.ext.asyncio import AsyncSession
 
 user_router = APIRouter()
+currentUser = Annotated[User, Depends(AuthService.get_current_user_from_token)]
+
 
 
 @user_router.post("/signup", status_code=201)
@@ -29,24 +32,19 @@ async def createUser(
 
 @user_router.delete("/profile", status_code=204)
 async def deleteUser(
-    db: AsyncSession = Depends(get_db),
-    current_user: User = Depends(AuthService.get_current_user_from_token),
+    current_user: currentUser, db: AsyncSession = Depends(get_db)
 ) -> None:
     await UserService.delete_user(current_user.user_id, db)
 
 
 @user_router.get("/profile", response_model=ShowUser, status_code=200)
-async def getUserById(
-    current_user: User = Depends(AuthService.get_current_user_from_token),
-) -> ShowUser:
+async def getUserById(current_user: currentUser) -> ShowUser:
     return current_user
 
 
 @user_router.patch("/profile", response_model=ShowUser, status_code=200)
 async def updateUserById(
-    body: UpdateUser,
-    db: AsyncSession = Depends(get_db),
-    current_user: User = Depends(AuthService.get_current_user_from_token),
+    body: UpdateUser, current_user: currentUser, db: AsyncSession = Depends(get_db)
 ) -> ShowUser:
     return await UserService.update_user_by_id(
         user_id=current_user.user_id, body=body, session=db
@@ -54,8 +52,6 @@ async def updateUserById(
 
 
 @user_router.get("/list", response_model=List[ShowUser], status_code=200)
-async def updateUserById(
-    db: AsyncSession = Depends(get_db),
-) -> List[ShowUser]:
+async def updateUserById(db: AsyncSession = Depends(get_db)) -> List[ShowUser]:
     user = await UserService.get_all_users(db)
     return user
